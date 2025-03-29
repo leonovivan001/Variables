b8b76569d9d1aa196b16e2ba06f46eb8be471f74
Иван Леонов <ivanleonov@macbook-air.local>
2025-03-29 17:25:34 +0400

    Собственно, тестирую

---
+++ src/main/kotlin/Task1.kt
@@ -0,0 +1,18 @@
+fun main () {
+
+    var number1: Byte = -111
+    var number2: Short = 18000
+    var number3: Int = 2_000_000_001
+    var number4: Long = 6_000_556_534_242_888_447
+    var number5: Float = 1.5f
+    var number6: Double = 1.6
+    var number7: Char = 'Ю'
+    var number8 = true //Boolean
+    var number9 = "Hello Everybody!"
+
+}
+
+/*
+9 переменных: (Byte, Short, Int, Long, Float, Double, Char, Boolean) и одну для String.
+Сразу присвой им какое-нибудь значение.
+*/
