b7bd8c0bf4db37776bedfaf2a0c14cf23b2b3aa9
ivanleonov <leonovivan001@gmail.com>
2025-03-30 14:35:15 +0400

    empty

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 fun main () {
 
+    var originalPrice: Double = 12000.0
+    var saleSize: Double = 20.0
+    var discountAmount = originalPrice * saleSize / 100
+    var finalPrice = originalPrice - discountAmount
+
+    println("Цена без скидки: $originalPrice")
+    println("Цена со скидкой: $finalPrice (скидка $saleSize%)")
 
 
 }
