797b5076a10bf457a51a3835f84db6be817afe65
ivanleonov <leonovivan001@gmail.com>
2025-03-30 14:49:03 +0400

    Задание завершено

--- src/main/kotlin/Task1.kt
+++ src/main/kotlin/Task1.kt
@@ -131,19 +131,4 @@ fun main () {
     println("После декремента $number6 => $DoubleResalt7")
     println()
 
-
-
-
-
-
-
-
-
-
-
 }
-
-/*
-9 переменных: (Byte, Short, Int, Long, Float, Double, Char, Boolean) и одну для String.
-Сразу присвой им какое-нибудь значение.
-*/
