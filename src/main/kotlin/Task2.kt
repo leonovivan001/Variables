d940bd4721762f9c50565aaa43a77b6e2bbf0494
ivanleonov <leonovivan001@gmail.com>
2025-03-30 14:37:46 +0400

    Задание завершено

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,5 +1,11 @@
 fun main () {
 
+    var originalPrice: Double = 12000.0
+    var saleSize: Double = 20.0
+    var discountAmount = originalPrice * saleSize / 100
+    var finalPrice = originalPrice - discountAmount
 
-// в процессе
+    println("Цена без скидки: $originalPrice")
+    println("Цена со скидкой: $finalPrice (скидка $saleSize%)")
+
 }
